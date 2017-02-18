0529024a987e03793c845e043ff5ff92f1d9d6aa
andrewpo456 <andrew.pope456@gmail.com>
2017-02-18 17:19:05 +1100

    Ability to remove quine directory if it already exists

--- quine.py
+++ quine.py
@@ -1,26 +1,33 @@
 import os
 import sys
-os.makedirs(os.getcwd() + '/quine')
+import shutil
+cwd = os.getcwd()
+if(os.path.isdir(cwd + '/quine')):
+    shutil.rmtree(cwd + '/quine')
+os.makedirs(cwd + '/quine')
 f = open('quine/quine.py', 'w')
 quine = [
   "import os",
   "import sys",
-  "os.makedirs(os.getcwd() + '/quine')",
+  "import shutil",
+  "cwd = os.getcwd()",
+  "if(os.path.isdir(cwd + '/quine')):",
+  "    shutil.rmtree(cwd + '/quine')",
+  "os.makedirs(cwd + '/quine')",
   "f = open('quine/quine.py', 'w')",
   "quine = [",
   "  ",
   "]",
-  "for i in range(0,5):",
+  "for i in range(0,9):",
   "    print(quine[i], file=f)",
   "for i in range(0,len(quine)):",
-  "    print(quine[5] + chr(34) + quine[i] + chr(34) + ',', file=f)",
-  "for i in range(6, len(quine)):",
-  "    print(quine[i], file=f)",
+  "    print(quine[9] + chr(34) + quine[i] + chr(34) + ',', file=f)",
+  "for i in range(10, len(quine)):",
+  "    print(quine[i], file=f)"
 ]
-for i in range(0,5):
+for i in range(0,9):
     print(quine[i], file=f)
 for i in range(0,len(quine)):
-    print(quine[5] + chr(34) + quine[i] + chr(34) + ',', file=f)
-for i in range(6, len(quine)):
+    print(quine[9] + chr(34) + quine[i] + chr(34) + ',', file=f)
+for i in range(10, len(quine)):
     print(quine[i], file=f)
-
